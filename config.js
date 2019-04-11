c7453d491a7195d580389f3fef54a1822cca9a86
Tjiho <tom.darboux@laposte.net>
2019-04-11 16:30:27 +0200

    Add support for env variables

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {}
 
 config.port = 8763
-config.server = "10.100.2.2"
-config.user = "darbouxtom"
+config.server = process.env.HDFSUP_TORUS_IP//"10.100.2.2"
+config.user = process.env.HDFSUP_TORUS_USERNAME
 
 module.exports = config
